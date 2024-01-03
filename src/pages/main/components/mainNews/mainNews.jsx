3dd5a36f6ce1400723a8a9d2d33cdd766e9fa92d
Artem Erusov <erusovav@gmail.com>
2024-01-03 09:55:13 +0300

    created the mainNews component

--- src/pages/main/components/mainNews/mainNews.jsx
+++ src/pages/main/components/mainNews/mainNews.jsx
@@ -1,11 +1,21 @@
 import styles from './styles.module.css';
+import IconArrow from '../../../../assets/css/svg/IconArrow';
+import MainImg1 from '../../../../assets/img/mainNews/mainNewsImg1.png';
+
 
 const MainNews = () => {
     return(
-        <main className={styles.main + ' ' + styles.container}>
-            press FFF
-        </main>
+        <div className={styles.container}>
+            <div className={styles.news}>
+                <p className={styles.news__title}>At the Chicago Architecture Biennial, Artists Run Free</p>
+                <button className={styles.news__btn}>
+                    <IconArrow/>
+                </button>
+            </div>
+            <p className={styles.date}>17 : 24 Friday, December 15, 2023</p>
+            <img className={styles.img} src={MainImg1} alt="" />
+        </div>
     )
-}
+};
 
 export default MainNews;
